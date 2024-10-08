40e5fe1789794d1993cfc6602b0f324fa42f4dd0
DaniilZinoviev05 <danielzinoviev@yandex.ru>
2024-10-08 03:07:54 +0300

    small change, refactoring

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 ###
-PACKAGES="timeshift grub-btrfs cronie viu mailutils mailx toilet"  
+PACKAGES="timeshift grub-btrfs mailutils mailx"  
 USER=$(whoami)
 ###
 #echo "Текущий пользователь: $USER"
@@ -10,7 +10,7 @@ echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
 
 install_libraries_arch() {
 	if ! pacman -Q $1 &> /dev/null; then
-		echo "Устанавливаю библиотеку $1..."
+		echo "Устанавливаем timeshift $1..."
 		sudo pacman -S $1 --noconfirm
 	else
 		echo "Библиотека $1 уже установлена."
@@ -26,4 +26,3 @@ for package in $PACKAGES; do
 done
 
 sudo rm /etc/sudoers.d/$USER
-
--- scripts/script.sh
+++ scripts/script.sh
@@ -1,25 +1,170 @@
 #! /bin/bash
 
-CONF="/home/$(whoami)/Scripts/setgs.conf"
-if [[ -f $CONF ]]; then
-	source "$CONF"
-	echo "User email / Почта пользователя: $user_email"
-fi
+### 
+CONF="/$(pwd)/../setgs.conf"
+###
+echo -e "\e[36m####################################################################################\e[0m"
 
-###
-echo "---------------------------------------------------------------------------"
+cat << "EOF"
+▗▄▄▖  ▗▄▖  ▗▄▄▖▗▖ ▗▖ ▗▄▖  ▗▄▄▖▗▄▄▄▖ ▗▄▄▖
+▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌▗▞▘▐▌ ▐▌▐▌   ▐▌   ▐▌   
+▐▛▀▘ ▐▛▀▜▌▐▌   ▐▛▚▖ ▐▛▀▜▌▐▌▝▜▌▐▛▀▀▘ ▝▀▚▖
+▐▌   ▐▌ ▐▌▝▚▄▄▖▐▌ ▐▌▐▌ ▐▌▝▚▄▞▘▐▙▄▄▖▗▄▄▞▘
+
+EOF
+
 echo "Install the required packages / Установим необходимы пакеты..."
-source /home/$(whoami)/Scripts/scripts/packages.sh
-echo "---------------------------------------------------------------------------"
+source /$(pwd)/packages.sh
 ###
-echo "---------------------------------------------------------------------------"
-echo "Let's make the settings / Произведем настройку..."
-source /home/$(whoami)/Scripts/scripts/settings.sh
-echo "---------------------------------------------------------------------------"
+echo -e "\e[36m####################################################################################\e[0m"
+
+cat << "EOF"
+ ▗▄▄▖▗▄▄▄▖▗▄▄▄▖▗▄▄▄▖▗▄▄▄▖▗▖  ▗▖ ▗▄▄▖ ▗▄▄▖
+▐▌   ▐▌     █    █    █  ▐▛▚▖▐▌▐▌   ▐▌   
+ ▝▀▚▖▐▛▀▀▘  █    █    █  ▐▌ ▝▜▌▐▌▝▜▌ ▝▀▚▖
+▗▄▄▞▘▐▙▄▄▖  █    █  ▗▄█▄▖▐▌  ▐▌▝▚▄▞▘▗▄▄▞▘                                          
+
+EOF
+
+source /$(pwd)/settings.sh
+echo -e "\e[36m####################################################################################\e[0m"
+###
+
+############## MAIN FUNCTIONS ##############
+createBackupFunc() {
+	clear
+	echo -e "En: \e[34mHere you can create a snapshot that will be automatically added to grub.\e[0m"
+	echo -e "Ru: \e[34mЗдесь Вы можете создать снимок системы, который сразу добавится в grub.\e[0m\n"
+	echo -e "\e[32m| 1 - Move on to create / Перейти к созданию\e[0m\n"
+	echo -e "\e[32m| 2 - Back / Назад\n\e[0m\n"
+	read -p "Enter value / Введите действие: " sub_action
+
+	case $sub_action in
+	1)
+		echo -e "\n"
+		read -p "Enter comment / Введите комментарий для бэкапа: " comment
+		sudo timeshift --create --comments "$comment"
+		sudo grub-mkconfig -o /boot/grub/grub.cfg
+		break
+	;;
+	2) 
+		break
+	;;
+	*)
+		echo -e "\e[31mInvalid value / Некорректный ввод \e[0m"
+	;;
+	esac
+	break
+}
+
+restoreBackupFunc() {
+	clear
+	echo -e "En: \e[34mHere you can restore your system.\e[0m"
+	echo -e "Ru: \e[34mЗдесь Вы можете восстановить систему.\e[0m\n"
+	echo -e "\e[32m| 1 - Move on to restore / Перейти к восстановлению системы\e[0m\n"
+	echo -e "\e[32m| 2 - Back / Назад\n\e[0m\n"
+	read -p "Enter value / Введите действие: " sub_action
+
+	case $sub_action in
+	1)
+		echo -e "\n"
+		echo -e "\e[32m$(sudo timeshift --list)\e[0m"
+		read -p "Enter backup name / Введите название бэкапа: " name
+		sudo timeshift --restore --snapshot $name
+		break
+	;;
+	2) 
+		break
+	;;
+	*)
+		echo -e "\e[31mInvalid value / Некорректный ввод \e[0m"
+	;;
+	esac
+}
+
+autoBackupFunc() {
+	clear
+	echo -e "En: \e[34mHere you can create autobackups.\e[0m"
+	echo -e "Ru: \e[34mЗдесь Вы можете настроить бэкапы по расписанию.\e[0m\n"
+	echo -e "\e[32m| 1 - Create autobackup / Создать автобэкап\e[0m\n" 
+	echo -e "\e[32m| 2 - Clear autobackups / Очистить бэкапы по расписанию\e[0m\n" 
+	echo -e "\e[32m| 3 - Back / Назад\n\e[0m\n"
+	read -p "Enter value / Введите действие: " sub_action
+	case $sub_action in
+		1)
+			clear
+			read -p "Comment / Введите комментарий для бэкапа: " comment
+			read -p "Month / Введите месяц (число, например, 9 для сентября): " month
+			read -p "Day of the week / Введите день недели (0 для воскресенья, 1 для понедельника и т.д.): " day
+			read -p "Enter time / Введите время (например, 12:45): " time
+
+			IFS=':' read -r hour minute <<< "$time"
+
+			if [[ ! $hour =~ ^[0-9]+$ ]] || [[ ! $minute =~ ^[0-9]+$ ]] || [[ "$hour" -lt 0 ]] || [[ "$hour" -gt 23 ]] || [[ "$minute" -lt 0 ]] || [[ "$minute" -gt 59 ]]; then
+			    echo "Incorrect time / Некорректное время."
+			    exit 1
+			fi
+			
+			crontab -l > cron
+			(crontab -l 2>/dev/null; echo "$minute $hour * $month $day sudo timeshift --create --comments \"$comment\"") >> cron
+			(crontab -l 2>/dev/null; echo "$minute $hour * $month $day sudo grub-mkconfig -o /boot/grub/grub.cfg") >> cron
+			(crontab -l 2>/dev/null; echo "$minute $hour * $month $day echo "Backup \"$comment\" created successfully / Бэкап \"$comment\" успешно создан." | mailx -s "BACKUP-REPORT" "$EMAIL"") >> cron
+			sudo crontab cron
+			rm cron
+			break
+		;;
+							
+		2)
+			echo -e "\n\e[34m### \e[32mCOMMANDS IN CRON(Script delete this commands)\e[0m / \e[32mКОМАНДЫ CRON(Скрипт удалит команды снизу)\e[34m ###\e[0m\n"
+			echo -e "\e[31m$(sudo crontab -l)\e[0m"
+			echo -e "\e[34m#################################################################################################\e[0m\n"
+			echo -e "\e[31m//////////////////////////////////////////////////////////////////\n"
+			sudo crontab -r
+			echo -e "\n//////////////////////////////////////////////////////////////////\e[0m"
+			echo -e "\n\e[34m### \e[32mCOMMANDS IN CRON(Now)\e[0m / \e[32mКОМАНДЫ CRON(Текущее состояние)\e[34m ###\e[0m\n"
+			echo -e "\e[31m$(sudo crontab -l)\e[0m"
+			echo -e "\e[34m###############################################################\e[0m\n"
+			sleep 10
+			break
+		;;
+		
+	       *)
+	       		echo "Incorrect value / Неверный ввод, попробуйте снова."
+	       		break
+	       	;;
+	esac
+}
+
+deleteBackupFunc() {
+	clear
+	echo -e "En: \e[34mHere you can delete your backup.\e[0m"
+	echo -e "Ru: \e[34mЗдесь Вы можете удалить бэкап.\e[0m\n"
+	echo -e "\e[32m| 1 - Move on to delete / Перейти к удалению\e[0m\n"
+	echo -e "\e[32m| 2 - Back / Назад\n\e[0m\n"
+	read -p "Enter value / Введите действие: " sub_action
+
+	case $sub_action in
+	1)
+		clear
+		echo -e "\e[34m################################## \e[32mBACKUPS\e[0m / \e[32mБЭКАПЫ\e[34m ##################################\e[0m"
+		echo -e "\e[32m$(sudo timeshift --list)\e[0m"
+		echo -e "\e[34m#####################################################################################\e[0m\n"
+		read -p "Enter backup name (name! Not comment!) / Введите название бэкапа (из поля name, не комментарий!): " name
+		sudo timeshift --delete --snapshot $name
+	;;
+	2) 
+		break
+	;;
+	*)
+		echo -e "\e[31mInvalid value / Некорректный ввод \e[0m"
+	;;
+	esac
+}
+######################################
 
 while true; do
 
-	viu  --width 60 -x 12 -y 1  /home/$(whoami)/Scripts/logo.jpg
+echo -e "\e[32m"
 
 cat << "EOF"
   _      _                                _        _                _                
@@ -32,100 +177,57 @@ cat << "EOF"
 	                                                                      |_|   
 EOF
 
-	(whoami)
-	echo -e "$(timeshift --version)\n"
+echo -e "\e[0m"
+
+	echo -e "\n\e[33mUser:\e[0m $(whoami) \e[34m|\e[0m \e[33mTimeshift:\e[0m $(timeshift --version)"
 	
-	{
-		echo -e "| 1 - Backups / Бэкапы |\n| 2 - Settings / Настройки |\n| 3  - Run / Запустить timeshift |\n| 4 - Exit / Выход |"
-	} | toilet -f term -F border --gay
-	
+	echo -e "\e[32m\n| 1 - Backups / Бэкапы\n\e[0m"
+	echo -e "\e[32m| 2 - Settings / Настройки\n\e[0m"
+	echo -e "\e[32m| 3 - Run / Запустить timeshift\n\e[0m"
+	echo -e "\e[32m| 4 - Exit / Выход\n\e[0m\n"
 	read -p "Enter value / Введите действие: " action
 	
+	while true; do
 	case $action in
 		1) 
 			clear
-			{
-				echo -e "| 1 - Create backup / Создать бэкап |\n| 2 - View backups / Посмотреть бэкапы |\n| 3 - Restore system / Восстановить систему |\n| 4 - Set up auto backups / Настроить автобэкапы |\n| 5 - Delete backup / Удалить бэкап |\n| 6 - Back / Назад |"
-			} | toilet -f term -F border --gay
-			read -p "Enter value / Введите действие: " action
+			echo -e "\e[32m\n| 1 - Create backup / Создать бэкап\n\e[0m"
+			echo -e "\e[32m| 2 - View backups / Посмотреть бэкапы\n\e[0m"
+			echo -e "\e[32m| 3 - Restore system / Восстановить систему\n\e[0m"
+			echo -e "\e[32m| 4 - Set up auto backups / Настроить автобэкапы\n\e[0m"
+			echo -e "\e[32m| 5 - Delete backup / Удалить бэкап\n\e[0m"
+			echo -e "\e[32m| 6 - Back / Назад\n\e[0m\n"
+			read -p "Enter value / Введите действие: " sub_action
 			
-			while true; do
-				case $action in
+				case $sub_action in
 				1) 
-					clear
-					read -p "Enter comment / Введите комментарий для бэкапа: " comment
-					sudo timeshift --create --comments $comment
-					sudo grub-mkconfig -o /boot/grub/grub.cfg
-					break
+					createBackupFunc
 				;;
-				
+
 				2) 
 					clear
+					echo -e "\e[34m################################## \e[32mBACKUPS\e[0m / \e[32mБЭКАПЫ\e[34m ##################################\e[0m"
 					sudo timeshift --list
+					echo -e "\e[34m#####################################################################################\e[0m"
 					break
 				;;
 				
 				3) 
-					clear
-					echo -e "\e[32m$(sudo timeshift --list)\e[0m"
-					read -p "Enter backup name / Введите название бэкапа: " name
-					sudo timeshift --restore --snapshot $name
-					break
+					restoreBackupFunc
 				;;
 				
 				4) 
-					clear
-					echo -e "1 - Create autobackup / Создать автобэкап" 
-					echo -e "2 - Clear autobackups / Очистить бэкапы по расписанию" 
-					read -p "Enter value / Введите действие: " action
-					case $action in
-						1)
-							clear
-							read -p "Comment / Введите комментарий для бэкапа: " comment
-							read -p "Month / Введите месяц (число, например, 9 для сентября): " month
-							read -p "Day of the week / Введите день недели (0 для воскресенья, 1 для понедельника и т.д.): " day
-							read -p "Enter time / Введите время (например, 12:45): " time
-
-							IFS=':' read -r hour minute <<< "$time"
-
-							if [[ ! $hour =~ ^[0-9]+$ ]] || [[ ! $minute =~ ^[0-9]+$ ]] || [[ "$hour" -lt 0 ]] || [[ "$hour" -gt 23 ]] || [[ "$minute" -lt 0 ]] || [[ "$minute" -gt 59 ]]; then
-							    echo "Incorrect time / Некорректное время."
-							    exit 1
-							fi
-							
-							(crontab -l 2>/dev/null; echo "$minute $hour * $month $day sudo timeshift --create --comments \"$comment\"") | crontab -  
-							(crontab -l 2>/dev/null; echo "$minute $hour * $month $day sudo grub-mkconfig -o /boot/grub/grub.cfg") | sudo crontab -  
-							if [[ -f $CONF ]]; then
-								source "$CONF"
-								(crontab -l 2>/dev/null; echo "$minute $hour * $month $day echo "Backup \"$comment\" created successfully / Бэкап \"$comment\" успешно создан." | mailx -s "BACKUP-REPORT" "$EMAIL"") | crontab -  
-							fi
-							break
-						;;
-											
-						2)
-							crontab -r 
-							break
-						;;
-						
-					       *)
-					       		echo "Incorrect value / Неверный ввод, попробуйте снова."
-					       		break
-					       	;;
-					esac
+					autoBackupFunc
 				;;
-				
 				5) 
-					clear
-					echo -e "\e[32m$(sudo timeshift --list)\e[0m"
-					read -p "Enter backup name / Введите название бэкапа (из поля name, не комментарий!): " name
-					sudo timeshift --delete --snapshot $name
+					deleteBackupFunc
 				;;
 				
 				6) 
+					clear
 					break
 				;;
 			esac
-		done
 		;;
 		2)
 			echo -e "\n1 - Change email / Изменить почту"
@@ -165,10 +267,13 @@ EOF
 		;;
 		3)
 			sudo timeshift-gtk
+			clear
+			break
 		;;
 		4)
 			exit
 		::
 		esac
+		done
 	
 done 
